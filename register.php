126a80e74231cd2412fee61aed0fcf19cddaa093
Dineth Chathurya <dinethchathurya2002@gmail.com>
2023-07-27 23:08:05 +0530

    connect the register php to html

--- register.php
+++ register.php
@@ -6,17 +6,19 @@ $dbname="NS";
 
 $conn=mysqli_connect($server,$usename,$password,$dbname);
 
-//$fname = $_REQUEST["fname"];
+$fname = $_REQUEST["fname"];
 $lname = $_REQUEST["lname"];
 $uname = $_REQUEST["uname"];
 $pass = $_REQUEST["pass"];
 $addres = $_REQUEST["address"];
+$zip=$_REQUEST["zip"];
 $tele = $_REQUEST["tele"];
+$email=$_REQUEST["email"];
 
 if (!$conn) {
     die("Connection faild: " . mysqli_connect_error());
 }
-$sql="INSERT INTO Client(Lname,username,password,address,number) VALUES('$lname','$uname','$pass','$addres','$tele')";
+$sql="INSERT INTO Client(Fname,Lname,username,password,address,number) VALUES('$fname','$lname','$uname','$pass','$addres','$tele')";
 
 if (mysqli_query($conn,$sql)) {
     echo"recoded";
