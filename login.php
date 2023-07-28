c1809cc49c6c29c9c75e371de13c72e550d8c66f
Dineth Chathurya <dinethchathurya2002@gmail.com>
2023-07-28 21:54:00 +0530

    do something special

--- login.php
+++ login.php
@@ -1,25 +1,36 @@
 <?php
-$server="localhost";
-$username="root";
-$password="root1234";
-$db="NS";
+$server = "localhost";
+$username = "root";
+$passwod = "root1234";
+$db = "NS";
 
-$conn=mysqli_connect($server, $username, $password, $db);
+$conn = mysqli_connect($server, $username, $passwod, $db);
 
-$email=$_REQUEST["email"];
-$pass=$_REQUEST["pass"];
+$email = $_REQUEST["email"];
+$pass = $_REQUEST["pass"];
 
 if (!$conn) {
-  die (mysqli_connect_error());
+  die(mysqli_connect_error());
 }
 
-$sql="SELECT * FROM Client WHERE email='$email'";
-$response=mysqli_query($conn,$sql);
-if($response) {
-    echo"";
+$sql = "SELECT * FROM Client WHERE email= '$email';";
 
+$result = mysqli_query($conn, $sql);
+$check = mysqli_num_rows($result);
+
+if ($check > 0) {
+  while ($row = mysqli_fetch_assoc($result)) {
+    if ($row['password'] == 'notuser123' && $email=='notuser@gmail.com') {
+     header("Location: admin.html");
+    }
+    else if ($row['password'] == $pass) {
+      header("Location: checkout.html");
+
+    } else
+      header("Location: login1.html");
+  }
 } else
-    echo "error";
+  header("Location: login1.html");
 
 mysqli_close($conn);
 ?>
